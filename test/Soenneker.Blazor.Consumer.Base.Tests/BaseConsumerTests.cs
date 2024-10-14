8379554c70d22508beb5d1a237c137d16ae29e96
Jake Soenneker <jake@soenneker.com>
2024-10-14 08:11:31 -0500

    Test fix

--- test/Soenneker.Blazor.Consumer.Base.Tests/BaseConsumerTests.cs
+++ test/Soenneker.Blazor.Consumer.Base.Tests/BaseConsumerTests.cs
@@ -1,6 +1,3 @@
-using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
-using Soenneker.Blazor.Consumer.Base.Abstract;
 using Soenneker.Tests.FixturedUnit;
 using Xunit;
 using Xunit.Abstractions;
@@ -10,10 +7,7 @@ namespace Soenneker.Blazor.Consumer.Base.Tests;
 [Collection("Collection")]
 public class BaseConsumerTests : FixturedUnitTest
 {
-    private readonly IBaseConsumer _util;
-
     public BaseConsumerTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
     {
-        _util = Resolve<IBaseConsumer>(true);
     }
 }
